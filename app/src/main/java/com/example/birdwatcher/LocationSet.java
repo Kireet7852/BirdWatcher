2d97191aae60379c4ed5e79894aaab9ae6fefc3f
Kireet7852 <kireetv100@gmail.com>
2023-03-28 22:34:13 +0530

    Add realtime database connection & save data

--- app/src/main/java/com/example/birdwatcher/LocationSet.java
+++ app/src/main/java/com/example/birdwatcher/LocationSet.java
@@ -3,6 +3,7 @@ package com.example.birdwatcher;
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +17,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,24 +25,40 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.birdwatcher.helpers.LocationTrack;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class LocationSet extends AppCompatActivity {
 
 //    FusedLocationProviderClient fusedLocationProviderClient;
     TextView latitude_label,longitude_label,address,city,country;
     TextView bird_spices, datetime, recognition_type, prediction;
-    Button getLocation;
+    Button getLocation, click_save;
     private final static int REQUEST_CODE = 100;
 //
 //    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+    DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference birdsRef = databaseRef.child("newBirdRef");
+    DatabaseReference newBirdRef = birdsRef.push();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
 
 
 
@@ -71,6 +89,7 @@ public class LocationSet extends AppCompatActivity {
         city = findViewById(R.id.city);
         country = findViewById(R.id.country);
         getLocation = findViewById(R.id.getLocation);
+        click_save = findViewById(R.id.save_button);
 
 //        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 //        getLocation.setOnClickListener(v -> getLastLocation());
@@ -137,6 +156,97 @@ public class LocationSet extends AppCompatActivity {
 
             }
         });
+
+
+        click_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                String bird_species_value = bird_spices.getText().toString();
+//                String datetime_value = datetime.getText().toString();
+//                String recognition_type_value = recognition_type.getText().toString();
+//                String prediction_value = prediction.getText().toString();
+//                String latitude_value = latitude_label.getText().toString();
+//                String longitude_value = longitude_label.getText().toString();
+//                String address_value = address.getText().toString();
+//                String city_value = city.getText().toString();
+//                String country_value = country.getText().toString();
+//
+//                // Create a new HashMap to hold the data
+//                Map<String, Object> birdInfo = new HashMap<>();
+//                birdInfo.put("bird_species", bird_species_value);
+//                birdInfo.put("datetime", datetime_value);
+//                birdInfo.put("recognition_type", recognition_type_value);
+//                birdInfo.put("prediction", prediction_value);
+//                birdInfo.put("latitude", latitude_value);
+//                birdInfo.put("longitude", longitude_value);
+//                birdInfo.put("address", address_value);
+//                birdInfo.put("city", city_value);
+//                birdInfo.put("country", country_value);
+//
+//                // Save the data to Firebase
+//                newBirdRef.setValue(birdInfo)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                Toast.makeText(LocationSet.this, "Data saved successfully", Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Toast.makeText(LocationSet.this, "Error saving data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+
+                // Get the text values from the TextViews
+                String birdSpecies = bird_spices.getText().toString();
+                String dateTime = datetime.getText().toString();
+                String recognitionType = recognition_type.getText().toString();
+                String predictionValue = prediction.getText().toString();
+                String latitude = latitude_label.getText().toString();
+                String longitude = longitude_label.getText().toString();
+                String addressValue = address.getText().toString();
+                String cityValue = city.getText().toString();
+                String countryValue = country.getText().toString();
+                String user_email = user.getEmail();
+
+                // Set the values to a new child node under the birdsRef
+//                newBirdRef.child("bird_species").setValue(birdSpecies);
+//                newBirdRef.child("date_time").setValue(dateTime);
+//                newBirdRef.child("recognition_type").setValue(recognitionType);
+//                newBirdRef.child("prediction").setValue(predictionValue);
+//                newBirdRef.child("latitude").setValue(latitude);
+//                newBirdRef.child("longitude").setValue(longitude);
+//                newBirdRef.child("address").setValue(addressValue);
+//                newBirdRef.child("city").setValue(cityValue);
+//                newBirdRef.child("country").setValue(countryValue);
+
+                if (TextUtils.isEmpty(birdSpecies) || TextUtils.isEmpty(dateTime) || TextUtils.isEmpty(recognitionType)
+                        || TextUtils.isEmpty(predictionValue) || TextUtils.isEmpty(latitude) || TextUtils.isEmpty(longitude)
+                        || TextUtils.isEmpty(addressValue) || TextUtils.isEmpty(cityValue) || TextUtils.isEmpty(countryValue)) {
+                    Toast.makeText(getApplicationContext(), "Please fill all fields! or click on location button", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    newBirdRef.child("bird_species").setValue(birdSpecies);
+                    newBirdRef.child("date_time").setValue(dateTime);
+                    newBirdRef.child("recognition_type").setValue(recognitionType);
+                    newBirdRef.child("prediction").setValue(predictionValue);
+                    newBirdRef.child("latitude").setValue(latitude);
+                    newBirdRef.child("longitude").setValue(longitude);
+                    newBirdRef.child("address").setValue(addressValue);
+                    newBirdRef.child("city").setValue(cityValue);
+                    newBirdRef.child("country").setValue(countryValue);
+                    newBirdRef.child("u_email").setValue(user_email);
+                    Toast.makeText(getApplicationContext(), "Bird saved successfully!", Toast.LENGTH_SHORT).show();
+//                    finish(); // close the activity
+                } catch (DatabaseException e) {
+                    Toast.makeText(getApplicationContext(), "Error saving bird: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 
